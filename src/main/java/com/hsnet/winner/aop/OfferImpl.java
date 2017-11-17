cc567388e88c09dffc4fbc2d60dffdb3cefed5b7
zhanggl <jsjxyzgl@163.com>
2017-11-17 15:16:37 +0800

    实现接口方法

--- src/main/java/com/hsnet/winner/aop/OfferImpl.java
+++ src/main/java/com/hsnet/winner/aop/OfferImpl.java
@@ -3,5 +3,15 @@ package com.hsnet.winner.aop;
 /**
  * Created by zhanggl on 2017/11/17.
  */
-public class OfferImpl  {
+public class OfferImpl implements IOffer {
+
+    @Override
+    public void postOffer() {
+
+    }
+
+    @Override
+    public void modifyOffer() {
+
+    }
 }
